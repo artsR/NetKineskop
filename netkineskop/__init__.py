617122eb9bc7df0519ae24e06f561cd044394eaf
artur <asihdi@gmail.com>
2020-10-07 08:06:25 +0200

    add auth routes

--- netkineskop/__init__.py
+++ netkineskop/__init__.py
@@ -49,6 +49,53 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirct('home')
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect('login')
+
+        login_user(user)
+        next_page = request.args.get('next', False)
+
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+
+        return redirect(next_page)
+
+    return render_template('auth.html', form=form, active='login')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+        flash('You Sign Up successfully!')
+        flash('Now you can Sign In.')
+        return redirect(url_for('login'))
+
+    return render_template('auth.html', form=form, active='register')
 
 
 @app.route('/subscriptions')
